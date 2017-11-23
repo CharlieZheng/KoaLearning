7c2694fb298bdf16b4d48fa95434b4f720ce3a75
hanrong <liuzheng625@live.com>
2017-11-23 16:50:31 +0800

    模拟耗时请求

--- demos/02.js
+++ demos/02.js
@@ -1,10 +1,26 @@
 const Koa = require('koa');
 const app = new Koa();
 
-const main = ctx => {
-  ctx.response.body = '{"Hello":"Hello"}';
-  ctx.response.type = "text/json"
+
+let sleep = async (seconds) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, seconds * 1000)
+    })
+}
+
+const lazy = async(ctx) => {
+    await sleep(4)
+    ctx.response.body = '{"Hello":"Hello"}';
+    ctx.response.type = "text/json"
+
+}
+const main = async function (ctx) {
+    await lazy(ctx)
 };
 
+
+// app.use(lazy);
 app.use(main);
 app.listen(3000);
