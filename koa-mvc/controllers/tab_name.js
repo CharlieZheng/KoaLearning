d784cccb5018c506c5106ae650213939c5653071
z <liuzheng625@live.com>
2019-06-11 19:03:03 +0800

    response 添加 headers

--- koa-mvc/controllers/tab_name.js
+++ koa-mvc/controllers/tab_name.js
@@ -2,14 +2,14 @@ const {query} = require("../data_source/base/query.js")
 let getTabList = async (ctx, next) => {
     let result = await query(`select * from tab_name`)
     result = JSON.stringify(result)
-    // result.forEach((
-    //     item
-    // ) =>{
-    //     item.
-    // })
     ctx.response.body = result
     ctx.response.code = 200
     ctx.response.type = "text/json"
+    // https://segmentfault.com/q/1010000009544146/
+    ctx.append('Access-Control-Allow-Origin', '*')
+    ctx.append('Access-Control-Request-Method', 'GET')
+
+
 }
 module.exports = {
     "GET /tab/tabList": getTabList
